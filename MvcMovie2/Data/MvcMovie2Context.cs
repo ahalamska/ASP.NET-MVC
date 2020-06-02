dfabf3ac5b20c3a74a2c740326c744edba436383
ahalamska <a.halamska@gmail.com>
2020-06-02 09:10:17 +0200

    Dodaj pliki projektów.

---
+++ MvcMovie2/Data/MvcMovie2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie2.Models;
+
+namespace MvcMovie2.Data
+{
+    public class MvcMovie2Context : DbContext
+    {
+        public MvcMovie2Context (DbContextOptions<MvcMovie2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie2.Models.Movie> Movie { get; set; }
+    }
+}
